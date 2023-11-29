bb64fd1358386359c26700b82093ef1a36c38cbf
Lizamie10 <152348245+Lizamie10@users.noreply.github.com>
2023-11-29 09:39:08 +0800

    Add files via upload

---
+++ Izamie/liza.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Find more emoji's here:https://www.webfx.com/tools/emoji_cheat_sheet/
+st.set_page_config(page_title="Who I am?", page_icon=":tada:", layout="wide")
+
+# ---- Header ----
+with st.container():
+    st.title("I am Lizamie G. Carangue")
+    st.subheader("18 years of age")
+    st.write("A Computer Engineering student in SNSU")
+    st.write("[Learn More >](https://www.facebook.com/smilyliza.carangue)")
